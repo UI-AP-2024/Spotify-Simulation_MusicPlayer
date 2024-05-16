49a6a935fe3c06bf26382f2a65eb8324cebcddda
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-05-17 02:50:59 +0330

    8th commit. home page(not logged in) added.

--- src/main/java/GraphicControllers/HomePageNotLoggedInController.java
+++ src/main/java/GraphicControllers/HomePageNotLoggedInController.java
@@ -1,4 +1,51 @@
 package GraphicControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 public class HomePageNotLoggedInController {
+
+    @FXML
+    private Button ArtistsSidebar;
+
+    @FXML
+    private ImageView HomeSideBar;
+
+    @FXML
+    private ImageView HomeSignUpBtn;
+
+    @FXML
+    private ImageView LibSidebar;
+
+    @FXML
+    private ImageView LoginBtn;
+
+    @FXML
+    private ImageView Searchsidebar;
+
+    @FXML
+    private Button audiosSidebar;
+
+    @FXML
+    private GridPane gridPaneHome;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private ScrollPane scrollPaneHome;
+
+    @FXML
+    private AnchorPane sideBar;
+
+    @FXML
+    void openLoginPage(MouseEvent event) {
+
+    }
+
 }
