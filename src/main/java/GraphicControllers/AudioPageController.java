014388168a0504a4f355e0c16d02e365add6bc62
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-05-18 02:09:31 +0330

    13th commit. Audio page added.

---
+++ src/main/java/GraphicControllers/AudioPageController.java
@@ -0,0 +1,66 @@
+package GraphicControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class AudioPageController {
+
+    @FXML
+    private Button ArtistsSidebar;
+
+    @FXML
+    private ImageView HomeSideBar;
+
+    @FXML
+    private ImageView LibSidebar;
+
+    @FXML
+    private ImageView Searchsidebar;
+
+    @FXML
+    private ImageView addBtn;
+
+    @FXML
+    private Text artistNameText;
+
+    @FXML
+    private Text audioTitleText;
+
+    @FXML
+    private Button audiosSidebar;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private ImageView coverImageView;
+
+    @FXML
+    private ImageView forBtn;
+
+    @FXML
+    private ImageView likeBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Text lyrics;
+
+    @FXML
+    private ImageView playPauseBtn;
+
+    @FXML
+    private TextField playlistNameText;
+
+    @FXML
+    private ImageView prevBtn;
+
+    @FXML
+    private AnchorPane sideBar;
+
+}
