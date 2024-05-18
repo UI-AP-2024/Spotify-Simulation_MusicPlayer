b1f2f8f2ece0bc47eec4d87ed7c56531971d8a36
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-05-18 15:49:46 +0330

    20th commit. Artist profile Added.

---
+++ src/main/java/GraphicControllers/ArtistProfilePageController.java
@@ -0,0 +1,51 @@
+package GraphicControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class ArtistProfilePageController {
+
+    @FXML
+    private Button ArtistsSidebar;
+
+    @FXML
+    private ListView<?> AudiosList;
+
+    @FXML
+    private ImageView HomeSideBar;
+
+    @FXML
+    private ImageView LibSidebar;
+
+    @FXML
+    private ImageView Searchsidebar;
+
+    @FXML
+    private Text artistNameText;
+
+    @FXML
+    private Button audiosSidebar;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Text bioText;
+
+    @FXML
+    private Button followBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button reportBtn;
+
+    @FXML
+    private AnchorPane sideBar;
+
+}
