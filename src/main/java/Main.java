dcc0eb67a68d2f734e02ac6c3ce575b3bd6770e3
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-05-18 15:52:51 +0330

    20th commit. Artist profile Added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,29 @@
 import controller.AdminController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import model.Admin;
 import view.View;
 
 import java.time.LocalDate;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) throws Exception {
         System.out.println("Hi there :)");
         // comment
         AdminController adminController = new AdminController();
         adminController.setAdmin(Admin.getAdmin("admin01", "adminPass", "Admin", "admin01@gmail.com","09121112233",LocalDate.of(2005,7,10)));
-        View.getView().showMenu();
+//        View.getView().showMenu();
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homePageNotLoggedIn.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 450);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
