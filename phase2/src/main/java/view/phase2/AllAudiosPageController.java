e718ded1473752bb227c5551ca1f367ce638b8f1
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-05-20 16:32:48 +0330

    29th commit. All Audios completed.

--- phase2/src/main/java/view/phase2/AllAudiosPageController.java
+++ phase2/src/main/java/view/phase2/AllAudiosPageController.java
@@ -2,22 +2,29 @@ package view.phase2;
 
 import controller.UserAccountController;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.Audio;
 import model.Database;
 
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class AllAudiosPageController implements Initializable {
+    public static Stage ctrlStage;
 
     @FXML
     private Button ArtistsSidebar;
@@ -49,6 +56,38 @@ public class AllAudiosPageController implements Initializable {
     @FXML
     private AnchorPane sideBar;
 
+    @FXML
+    void allArtistsAct(MouseEvent event) {
+        ArtistProfilePageController.allAudiosSideBar(ctrlStage);
+    }
+
+    @FXML
+    void backAct(MouseEvent event) {
+        SearchController.backMethodLogged(ctrlStage);
+    }
+
+    @FXML
+    void homeAct(MouseEvent event) {
+        AllArtistsPagesController.loadHomeLogged(ctrlStage);
+    }
+
+    @FXML
+    void libAct(MouseEvent event) {
+        HomePageLoggedInController.libraryMethod(ctrlStage);
+    }
+
+    @FXML
+    void logOutAct(MouseEvent event) {
+        HomePageLoggedInController.logoutMethod(ctrlStage);
+    }
+
+    @FXML
+    void searchAct(MouseEvent event) {
+        AllArtistsPagesController.searchActionSide(ctrlStage);
+    }
+
+    public static Audio chosenAudio;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         int counter = 0;
@@ -65,6 +104,19 @@ public class AllAudiosPageController implements Initializable {
                 Label label = new Label(Database.getDatabase().getAllAudios().get(counter).getAudioTitle());
                 label.setTranslateY(20);
                 label.setTranslateX(25);
+                int finalCounter = counter;
+                mainVBox.setOnMouseClicked(event -> {
+                    chosenAudio = Database.getDatabase().getAllAudios().get(finalCounter);
+                    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Audio-page.fxml"));
+                    Scene scene = null;
+                    try {
+                        scene = new Scene(fxmlLoader.load(), 600, 470);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    ctrlStage.setScene(scene);
+                    ctrlStage.show();
+                });
                 counter++;
                 mainVBox.getChildren().addAll(imageView, label);
                 gridpane.add(mainVBox, j, i);
--- phase2/src/main/java/view/phase2/Main.java
+++ phase2/src/main/java/view/phase2/Main.java
@@ -101,6 +101,7 @@ public class Main extends Application {
         AllArtistsPagesController.ctrlStage = stage;
         HomePageLoggedInController.ctrlStage = stage;
         SearchController.ctrlStage = stage;
+        AllAudiosPageController.ctrlStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homePageNotLoggedIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 450);
         stage.setTitle("Spotify");
